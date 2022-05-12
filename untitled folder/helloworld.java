a5bf9f02af7f72bd9757c9d52916a1a6c6797906
shyampatel <shyampatel.55555@gmail.com>
2022-05-12 17:30:27 +0530

    hello

---
+++ untitled folder/helloworld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+        System.out.println("hello");
+    }
+}
---
+++ untitled folder/primitve.java
@@ -0,0 +1,8 @@
+
+public class primitive{
+    public static void main(String args[]){
+        int roll=34;
+        char letter='r';
+
+    }
+}
