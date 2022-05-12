aadd80f5e5d25d506badec2872a00e2754e64000
shyampatel <shyampatel.55555@gmail.com>
2022-05-12 17:57:11 +0530

    array highest num

--- untitled folder/helloworld.java
+++ untitled folder/helloworld.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("hello");
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine());
-        System.out.println("hello");
+        int [] arr={1,3,29,77};
+        System.out.println(maxRange(arr,0,3));
+    }
+    static int maxRange(int[] arr, int start , int end){
+        int maxval=arr[start];
+        for(int y=start; y<=end; y++){
+            if(arr[y]>maxval){
+                maxval=arr[y];
+            }
+        }
+        return maxval;
     }
 }
