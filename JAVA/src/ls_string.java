a16c99dea57a151dcb861dae35d75fd4bf82620d
shyampatel <shyampatel.55555@gmail.com>
2022-05-15 19:15:42 +0530

    linearsearchstring

--- JAVA/src/ls_string.java
+++ JAVA/src/ls_string.java
@@ -1,5 +1,20 @@
 public class ls_string {
     public static void main(String[] args) {
-
+        String x="Shyam";
+        char t='z';
+        System.out.println(lsstring(x,t));
+    }
+    static boolean lsstring(String x , char t){
+        if(x.length()==0){
+            return false;
+        }
+        else{
+            for(int f=0;f<x.length();f++){
+                if(x.charAt(f) == t){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
