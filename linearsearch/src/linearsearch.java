af7b90a67717937afe6fffcca03e2b08a3f890b1
shyampatel <shyampatel.55555@gmail.com>
2022-05-15 15:31:56 +0530

    Array

---
+++ linearsearch/src/linearsearch.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class linearsearch {
+    public static void main(String[] args) {
+       int [] arr = {24,45,56,78};
+       arr = reverse(arr);
+       System.out.println(Arrays.toString(arr));
+    }
+    static int [] reverse(int [] arr) {
+            int start=0;
+            int end=arr.length-1;
+            while(start < end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+            return arr;
+    }
+    static void swap(int [] arr, int a, int b){
+        int t;
+        t=arr[a];
+        arr[a]=arr[b];
+        arr[b]=t;
+    }
+}
