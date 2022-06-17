b251719652846c77ece0863bb1139275ea66daf1
shyampatel <shyampatel.55555@gmail.com>
2022-06-17 22:47:53 +0530

    sort_cyclicsort_missingnum

--- JAVA/src/sort_cyclicsort.java
+++ JAVA/src/sort_cyclicsort.java
@@ -7,10 +7,19 @@ public class sort_cyclicsort {
         System.out.println(Arrays.toString(arr));
     }
     static void cyclicsort(int []arr){
-        for(int i = 0; i<arr.length;i++){
-            if(arr[i]!=i+1){
-                arr[i]=i+1;
+        int i=0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i] !=arr[correct]){
+                int temp;
+                temp = arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+            else{
+                i++;
             }
         }
+
     }
 }
